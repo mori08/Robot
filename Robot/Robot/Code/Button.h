c05bb4e5eef7c0059cc2882b1039655dc3e95a9f
森本 <mrmt0814@gmail.com>
2018-11-28 23:38:00 +0900

    InputManagerとButtonを定義

---
+++ Robot/Robot/Code/Button.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+namespace Robot
+{
+	class Button
+	{
+	private:
+
+	public:
+
+	};
+}
---
+++ Robot/Robot/Code/InputManager.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+namespace Robot
+{
+
+	class InputManager
+	{
+	private:
+
+	public:
+
+	};
+
+}
