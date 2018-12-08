c610e9ec92b03c6ceb43159a24c685239ff249ae
森本 <mrmt0814@gmail.com>
2018-12-08 20:30:33 +0900

    タイトルロゴの実装

--- Robot/Robot/Code/Main.cpp
+++ Robot/Robot/Code/Main.cpp
@@ -4,8 +4,13 @@
 #include"Title\TitleScene.h"
 
 
+void asseter(const String & dirname);
+
+
 void Main()
 {
+	asseter(L"Asset/");
+
 	MyApp sceneManager;
 
 	sceneManager.add<Robot::TitleScene>(L"TitleScene");
@@ -15,5 +20,44 @@ void Main()
 		sceneManager.updateAndDraw();
 
 		Robot::InputManager::Instance().changeState();
+	}
+}
+
+
+void asseter(const String & direname)
+{
+	// 指定されたディレクトリのファイルパスを配列に
+	Array<FilePath> contents = FileSystem::DirectoryContents(direname);
+
+	for (const auto& content : contents)
+	{
+
+		String filename = FileSystem::FileName(content);
+
+
+		// 指定されたパスがディレクトリであるとき
+		if (FileSystem::IsDirectory(content))
+		{
+			asseter(direname + filename + L"/"); // 関数を再び呼ぶ
+		}
+
+
+		// 指定されたパスがファイルであるとき
+		else if (FileSystem::IsFile(content))
+		{
+
+			// 拡張子がpngのとき
+			if (FileSystem::Extension(content) == L"png")
+			{
+				TextureAsset::Register(FileSystem::BaseName(content), direname + filename);
+			}
+
+			// 拡張子がmp3のとき
+			else if (FileSystem::Extension(content) == L"mp3")
+			{
+				SoundAsset::Register(FileSystem::BaseName(content), direname + filename);
+			}
+
+		}
 	}
 }
--- Robot/Robot/Code/Title/TitleScene.cpp
+++ Robot/Robot/Code/Title/TitleScene.cpp
@@ -4,6 +4,10 @@
 
 namespace
 {
+	const Color BLACK(15);  // 黒
+	const Color GRAY(125);  // 灰
+	const Color WHITE(240); // 白
+
 	const String ENTER_KEY(L"enter"); // 始めるボタンのキー
 	const String EXIT_KEY(L"exit");   // 終えるボタンのキー
 
@@ -11,6 +15,11 @@ namespace
 	const Point EXIT_BUTTON_POS(420, 370);  // 終えるボタンの座標
 
 	const Size  BUTTON_SIZE(160, 70);       // ボタンのサイズ
+
+	const Point TITLE_LOGO_POS(80, 120); // タイトルロゴの座標
+
+	const double SHADOW_BLUR_RADIUS = 20.0; // 影のぼかしの大きさ
+	const double SHADOW_SPREAD      = 8.0;  // 影の広がりかた
 }
 
 
@@ -41,5 +50,10 @@ void Robot::TitleScene::update()
 
 void Robot::TitleScene::draw() const
 {
-	InputManager::Instance().getSelectedButton().getRegion().draw();
+	Window::ClientRect().draw(Color(BLACK));
+
+	Rect(TITLE_LOGO_POS, TextureAsset(L"TitleLogo").size).drawShadow(Vec2::Zero, SHADOW_BLUR_RADIUS, SHADOW_SPREAD, GRAY);
+	TextureAsset(L"TitleLogo").draw(TITLE_LOGO_POS);
+	
+	InputManager::Instance().getSelectedButton().getRegion().drawFrame(1, 1, WHITE);
 }
