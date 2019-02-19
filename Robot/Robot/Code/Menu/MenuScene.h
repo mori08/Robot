c4d3826bcc50efb4ac5924454b1616b3875c8963
森本 <mrmt0814@gmail.com>
2019-02-19 16:59:46 +0900

    MenuSceneクラスの宣言

---
+++ Robot/Robot/Code/Menu/MenuScene.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+#include "../Main.h"
+
+
+namespace Robot
+{
+	/*
+	MenuSceneƒNƒ‰ƒX
+	ButtonList‚ğŠÇ—‚µ
+	Menu‚ğ‘€ì‚µ‚Ü‚·
+	*/
+	class MenuScene : public MyApp::Scene
+	{
+	private:
+
+		
+
+	public:
+
+	private:
+
+		void update() override;
+
+		void draw() const override;
+
+	};
+}
